36431c7cd6555c4c6eebc77bdbf73f70c399e3c6
yashmandi <132436131+yashmandi@users.noreply.github.com>
2024-01-04 13:59:23 +0000

    RomanToInt.java

---
+++ RomanToInt.java
@@ -0,0 +1,46 @@
+public class RomanToInt {
+    public static void main(String[] args) {
+        RomanToInt roman = new RomanToInt();
+        System.out.println(roman.romanToInt("III"));
+    }
+
+    static int getVal(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+
+    public static int romanToInt(String s) {
+        int sum = 0;
+        int cur = getVal(s.charAt(0));
+
+        for (int i = 1; i < s.length(); i++) {
+            int next = getVal(s.charAt(i));
+
+            if (cur < next) {
+                sum -= cur;
+            } else {
+                sum += cur;
+            }
+            sum += cur;
+        }
+        sum += cur;
+        return sum;
+
+    }
+}
