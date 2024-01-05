f80b5512ad2359c71a15624ae262019b08390dd4
yashmandi <yashmandi18@gmail.com>
2024-01-05 17:23:23 +0530

    MergeTwoLists

---
+++ MergeTwoLists.java
@@ -0,0 +1,26 @@
+public class MergeTwoLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (l1 != null && l1 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        if (l1 != null) {
+            curr.next = l1;
+            l1 = l1.next;
+        }
+        if (l2 != null) {
+            curr.next = l2;
+            l2 = l2.next;
+        }
+        return dummy.next;
+    }
+}
