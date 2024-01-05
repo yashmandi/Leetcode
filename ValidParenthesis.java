71578f5d5658cf2efb93e72038add828f705371f
yashmandi <yashmandi18@gmail.com>
2024-01-05 17:00:13 +0530

    Valid Parenthesis

---
+++ ValidParenthesis.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public static void main(String[] args) {
+        String s = "()[]{}";
+        System.out.println(isValid(s));
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((c == ')' && top == '(') ||
+                        (c == ']' && top == '[') ||
+                        (c == '}' && top == '{')) {
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+}
